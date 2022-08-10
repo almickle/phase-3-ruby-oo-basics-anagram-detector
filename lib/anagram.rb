904f3dc32d7d84bddd3a598ed4c023a19a893eb6
Michael Dear <michaelcdear@gmail.com>
2022-08-10 12:11:32 -0600

    commit tree d3080138d75e48ab5aa616ecbd1c4cd9cbf4683f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+    def initialize(word="listen")
+        @word = word
+        @match = []
+    end
+    def match(array)
+        array.each do |word|
+            word.split("").sort
+            if word.split("").sort == @word.split("").sort
+                @match.push(word)
+            end
+        end
+        @match
+    end
+end
+
+myAnagram = Anagram.new
+myAnagram.match(%w[enlists google inlets banana])
